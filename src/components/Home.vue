84cee8289d1b07e958d5ebb69914405cc4f74dc3
guoyihua <1573338993@qq.com>
2021-04-29 21:09:08 +0800

    dev

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,98 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/mainPage.png" alt="logo" />
+        <span>manage</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-user"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item index="1-1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span> 
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data: function() {
+    return {
+      menuList: []
+    };
+  },
+  created() {
+    this.getMenuList();
+  },
   methods: {
     logout: function() {
       window.sessionStorage.clear();
       this.$router.push('/login');
+    },
+    getMenuList: async function() {
+      const { data: res } = await this.$http.get('menus');
+      if (res.meta.status !== 200) {
+        this.$message.error(res.meta.msg);
+      }
+      this.menuList = res.data;
     }
   }
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+
+.el-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #373d41;
+  color: #fff;
+  font-size: 22px;
+  > div {
+    display: flex;
+    align-items: center;
+    img {
+      height: 40px;
+    }
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+</style>
