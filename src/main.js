09c74e1720b9ccd437ca44e9d8135e8fbb8f02d4
guoyihua <1573338993@qq.com>
2021-04-29 21:09:08 +0800

    dev

--- src/main.js
+++ src/main.js
@@ -1,26 +1,32 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
-import App from "./App";
-import router from "./router";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-import "./assets/css/global.css";
-import "./assets/font/iconfont.css";
-import axios from "axios";
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/css/global.css';
+import './assets/font/iconfont.css';
+import axios from 'axios';
 
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
-Vue.prototype.$http = axios;
-
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';
 
+// 配置请求拦截器
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token');
+  return config;
+});
+
+Vue.prototype.$http = axios;
+
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
+  el: '#app',
   router,
   components: { App },
-  template: "<App/>"
+  template: '<App/>'
 });
